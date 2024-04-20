3adf7fc24872757e10f62528cd993be566cf3c95
feloxes <155644281+feloxes@users.noreply.github.com>
2024-04-20 11:07:27 +0200

    Submitted version
    May have changed comments before submitting, but nothing major

--- B4.m
+++ B4.m
@@ -2,80 +2,82 @@ clc
 clear
 close all
 
-global Fin_l Fin_g V_gas_initial V_initial ...
-       yo2_in yco2_in He P T R...
-       S0 mu_max kLa Ks ...
-       Yxs Yos Ycs Ysx
+global  Fin_l Fin_g V_g_i V_l_i ...
+        yo2_in yco2_in ci_o2 He P T R ... 
+        S0 mu_max kLa Ks  ...
+        Yxs Yos Ycs Ysx 
 
-    t= [0 36]; %h, may also use: linspace(0, 36, 1000);
-    
-    V_initial=0.5; % L
-    V_gas_initial= 9.5; %L
-    
-    Fin_l=0.2; % L/h
-    Fin_g=60;   % L/h
-    
-    S0= 25; % g/L
-    Si=5;   % g/L
-    yo2_in=0.2095;
-    yco2_in=0.0004;
-    
-    Xi=0.5;  % g/L
-    ci_o2=265*10^-6; % M
-    
-    mu_max=0.5; % /h
-    Ks=0.5; % g/h
-    Yxs=0.5; % g/g
-    Ysx=1/Yxs;
-    Yos=0.01; % mol/g
-    Ycs=0.01; % mol/g
-    
-    kLa=500; % /h
-    He=790; % atm*L/mol
-    P=1; % atm 
-    %P=101325;  % Pa
-    T=293;  % K
-    R=0.08206; % atm*L/mol*K
-    
-    
-    
-    starting_conditions= [Xi, Si, ci_o2, yo2_in, yco2_in];
+    t = linspace(0, 36, 1000); % cultivation period (h)
 
+    V_l_i   = 0.5;             % initial liquid volume (L)
+    V_g_i   = 9.5;             % initial gas volume (L)
+    
+    Fin_l   = 0.02;             % liquid flow rate (L/h)
+    Fin_g   = 60;              % gas flow rate (L/h) 
+
+    VL      = V_l_i+Fin_l*t;   % liquid volume, at time t (L) 
+    VG      = V_g_i-Fin_l*t;   % gas volume, at time t (L)
+
+    S0      = 100;              % substrate concentration in inlet (g/L)
+    Si      = 5;               % initial substrate concentration (g/L)
+    yo2_in  = 0.2095;          % mole fraction of O2 in inlet
+    yco2_in = 0.0004;          % mole fraction of CO2 in inlet 
+    
+    Xi      = 0.5;             % initial biomass concentration (g/L)
+    ci_o2   = 265e-6;          % initial O2 concentration, liquid (M) 
+    
+    mu_max  = 0.5;             % maximum specific growth rate (/h)
+    Ks      = 0.5;             % substrate saturation constant (g/L)
+    Yxs     = 0.5;             % biomass yield (g biomass/g substrate)
+    Ysx     = 1/Yxs;           % substrate yield (g substrate/ g biomass)
+    Yos     = 0.01;            % oxygen yield (mol/g) 
+    Ycs     = 0.01;            % carbon dioxide yield (mol/g)
+    
+    kLa     = 500;             % oxygen mass transfer coefficient (/h)
+    He      = 790;             % henry's constant for oxygen, (atm*L/mol) 
+    P       = 1;               % pressure (atm)
+    T       = 293;             % temperature (K)
+    R       = 0.08206;         % ideal gas constant (atm*L/mol*K)
+    
+    starting_conditions = [Xi, Si, ci_o2, yo2_in, yco2_in];
+    
     [t, y]=ode15s(@(t,y)B4_fun(t, y), t, starting_conditions);
-    
-    
+      
     X= y(:,1);
     S= y(:,2);
     c_o2= y(:,3);
     y_o2g= y(:,4);
     y_co2g= y(:,5);
 
-subplot(2, 2, 1)
-fontScaling = 1.2;
-plot(t,X, ...
-    t, S)
+subplot(2, 2, 1)    
+plot(t,X,t,S,'LineWidth',0.75)
 title("Concentration of X and S")
-legend("X", "S")
-ylabel("c [g/L]")
-%fontsize(gcf, scale=fontScaling)
+legend("biomass concentration, X", "substrate concentration, S")
+ylabel('concentration [g/L]')
+xlabel('time [h]')
+xlim([0 36])
 
-subplot(2, 2, 2)
-plot(t, c_o2*1e6)
-ylabel("c(O2) [µ mol]")
-fontsize(gcf, scale=fontScaling)
+subplot(2, 2, 2)    
+plot(t,c_o2*10^6,'LineWidth',0.75) % ändra skala, är det mikro nu?
+title("Oxygen concentration in the liquid")
+legend("c_O_2_,_l")
+ylabel('concentration [μM]') 
+xlabel('time [h]')
+xlim([0 36])
 
-subplot(2, 2, 3)
-plot(t, y_o2g, t, y_co2g)
-title("Molar fractions of O2 and CO2")
-legend("y(O2)", "y(CO2)")
-fontsize(gcf, scale=fontScaling)
+subplot(2, 2, 3)    
+plot(t,y_o2g,t,y_co2g,'LineWidth',0.75)  
+title("Mole fractions of oxygen and carbon dioxide in outlet")
+legend('y_O_2_,_g','y_C_O_2_,_g')
+ylabel('mole fraction') 
+xlabel('time [h]')
+xlim([0 36])
 
+subplot(2, 2, 4)    
+plot(t,VL,t,VG,'LineWidth',0.75) 
+title("Volume")
+legend('liquid','gas')
+ylabel('volume [L]') 
+xlabel('time [h]')
+xlim([0 36])
 
-V_plot = V_initial+Fin_l*t;
-V_gas_plot=V_gas_initial-Fin_l*t;
-V_total_plot = V_plot+V_gas_plot;
-subplot(2, 2, 4)
-plot(t, V_plot, t, V_gas_plot)
-ylabel("Volume [L]")
-legend("Liquid Volume", "Gas Volume")
-fontsize(gcf, scale=fontScaling)
--- B4_fun.m
+++ B4_fun.m
@@ -1,39 +1,36 @@
-function dydt = B42_fun(t, y)
+function dydt = B4_fun(t, y)
 
-global Fin_l Fin_g V_gas_initial V_initial ...
-       yo2_in yco2_in He P T R...
+global Fin_l Fin_g V_g_i V_l_i ...
+       yo2_in yco2_in ci_o2 He P T R...
        S0 mu_max kLa Ks ...
        Yxs Yos Ycs Ysx
 
-    X = y(1);
-    S = y(2);
-    c_o2 = y(3);
-    yo2 = y(4);
-    yco2 = y(5);
+    X       = y(1);
+    S       = y(2);
+    c_o2    = y(3);
+    yo2     = y(4);
+    yco2    = y(5);
 
+    VL      = V_l_i+Fin_l*t;
+    VG      = V_g_i-Fin_l*t;
 
-    VL=V_initial+Fin_l*t;
-    VG=V_gas_initial-Fin_l*t;
+    if VL>10
+        VL = 10; 
+        VG = 0; 
+        Fin_l=0;
+        Fin_g=0;
+    end
 
-if VL>=10
-    Fin_l=0;
-    Fin_g=0;
-end
+    mu      = mu_max*S/(Ks+S);                   % specific growth rate (/h)
+    q_o2    = -Yos/Yxs*mu;                       % specific rate of oxygen consumption (mol/g*h)
+    q_co2   = Ycs/Yxs*mu;                        % specific rate of carbon dioxide production (mol/g*h)   
 
-
-    mu=mu_max*S/(Ks+S);
-    q_o2=kLa*(yo2_in-yo2)/(VL*X);
-   % q_co2=-kLa*(yo2_in-yo2)/(VL*X); OLD
-    q_co2= Ycs/Yxs*mu;
-
-    dV_Ldt = Fin_l;          % change in liquid volume
-    dV_Gdt = -Fin_l;         % change in gas volume
-    dXdt = -Fin_l/VL*X+mu*X;    % change in X
-    dsdt =  Fin_l/VL*(S0-S)-Ysx*mu*X;      % substrate consumption rate
-    dc_o2dt = q_o2*X+kLa*((yo2*P)/He-c_o2);     % O2 in liquid phase
-    dyo2dt =  Fin_g/VG*(yo2_in-yo2*(1-yo2_in-yco2_in)/(1-yo2-yco2))-kLa*(yo2*P/He-c_o2)*VL*R*T/(VG*P);   %  O2 in gas 
-    dyco2dt = Fin_g/VG*(yco2_in-yco2*(1-yo2_in-yco2_in)/(1-yo2-yco2))+q_co2*X*VL*R*T/(VG*P);    %  CO2 i gas
-
-   %dV_Ldt; dV_Gdt;
-    dydt=[dXdt; dsdt; dc_o2dt; dyo2dt; dyco2dt];
+    dV_Ldt  = Fin_l;                             % change in liquid volume
+    dV_Gdt  = -Fin_l;                            % change in gas volume 
+    dXdt    = -Fin_l/VL*X+mu*X;                  % change in biomass concentration
+    dsdt    = Fin_l/VL*(S0-S)-Ysx*mu*X;          % change in substrate concentration 
+    dc_o2dt = q_o2*X+kLa*(yo2*P/He-c_o2);        % change in oxygen cocnentration in the liquid phase
+    dyo2dt  = Fin_g/VG*(yo2_in-yo2*(1-yo2_in-yco2_in)/(1-yo2-yco2))-kLa*(yo2*P/He-c_o2)*VL*R*T/(VG*P);   %  change in mole fraction of oxygen  
+    dyco2dt = Fin_g/VG*(yco2_in-yco2*(1-yo2_in-yco2_in)/(1-yo2-yco2))+q_co2*X*VL*R*T/(VG*P);             %  change in mole fraction of carbon dioxide
+    dydt    = [dXdt; dsdt; dc_o2dt; dyo2dt; dyco2dt];
     
